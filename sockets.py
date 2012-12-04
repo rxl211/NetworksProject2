14e0ccd5ab92354e182ae5848bdeca50fb9064ce
Rafael Lopez <rafael@case.edu>
2012-12-04 17:46:23 -0500

    more or less a complete traceroute using standard ttl+1
    Signed-off-by: Rafael Lopez <rafael@case.edu>

--- sockets.py
+++ sockets.py
@@ -1,5 +1,6 @@
 from socket import *
 from struct import *
+from random import randint
 import sys
 import select
 import time
@@ -17,7 +18,7 @@ assert UDP_CODE == IPPROTO_UDP, "UDP Protocol type mismatch!"
 
 HOST = gethostbyname(gethostname())
 
-def construct_ip_header(hostname):
+def construct_ip_header(hostip, ttl, ipid):
   '''
                 8 bits        8 bits                   16 bits               = 32 bits
             |--------------------------------------------------------------|
@@ -34,24 +35,23 @@ def construct_ip_header(hostname):
     '''
 
   source_ip = "129.22.59.79"
-  dest_ip = gethostbyname(hostname)
 
   ip_ver = 4 #IPv4 baby -> 4 bits
   ip_ihl = 5 #5 words, 20 bytes (no options) -> 4 bits
   ip_tos = 0 #pretty sure this isn't really used much -> 8 bits
   ip_tot_len = 0	# kernel will fill this in (<3)
-  ip_id = 1337	#packet id -> 16 bits
+  ip_id = ipid	#packet id -> 16 bits
   ip_frag_off = 0 #fragmentation offset -> 16 bits
-  ip_ttl = 5 # reduced by one on each hop -> 8 bits
+  ip_ttl = ttl # reduced by one on each hop -> 8 bits
   ip_proto = UDP_CODE #wooo! -> 8 bits
   ip_checksum = 0	# kernel will fill this in (<3)
-  ip_source = inet_aton(source_ip)	#convert x.x.x.x to 32-bit packed binary format -> 32 bits
-  ip_dest = inet_aton(dest_ip) #convert x.x.x.x to 32-bit packed binary format -> 32 bits
+  ip_source = inet_aton(source_ip)	#convert x.x.x.x to 32-bit packed binary
+  ip_dest = inet_aton(hostip) #convert x.x.x.x to 32-bit packed binary
 
   ip_ihlver = (ip_ver << 4) + ip_ihl #combine to get 8 bits
 
   '''
-   ! for network order (big endian)
+   ! for network order (big endian)orig_udp_header
    B for unsigned char -> 8 bits
    H for unsigned short -> 16 bits
    4s for 4 byte (32 bit) string
@@ -76,61 +76,109 @@ def construct_udp_packet():
   payload = '' #we don't actually care to send data
   return udp_header + payload
 
-def create_packet(hostname):
-  ip_header = construct_ip_header(hostname)
+def create_packet(hostip, ttl, ipid):
+  ip_header = construct_ip_header(hostip, ttl, ipid)
   payload = construct_udp_packet()
   return ip_header + payload
 
-def await_response(my_socket, time_sent, timeout):
-  time_left = timeout
+def validate_ip(orig_ip_header, hostip, ipid, ttl):
+  orig_len = orig_ip_header[2]
+  orig_id = orig_ip_header[3]
+  orig_proto = orig_ip_header[6]
+  orig_dest = inet_ntoa(orig_ip_header[9])
 
+  return (orig_len == 28 and orig_id == ipid and orig_proto == UDP_CODE
+          and orig_dest == hostip)
+
+def validate_udp(orig_udp_header):
+  orig_source = orig_udp_header[0]
+  orig_dest = orig_udp_header[1]
+  orig_len = orig_udp_header[2]
+
+  return (orig_source == 20591 and orig_dest == 33433 and orig_len == 8)
+
+def await_response(my_socket, hostip, ipid, ttl, time_sent, timeout):
+  time_left = timeout
+  time_start = time.time()
   while True:
-    rec_packet = my_socket.recv(5120)
-    print "we passed read"
-    unpacked_ip = unpack('!BBHHHBBH4s4s', rec_packet[0:19]) #0:20
+    valid_icmp = False
+    valid_ip = False
+    time_before = time.time()
+    try:
+      rec_packet = my_socket.recv(5120)
+    except:
+      time_left = time_left - timeout
+      if time_left <= 0:
+        raise Exception
+
+    timediff = time.time() - time_before
+
+    unpacked_ip = unpack('!BBHHHBBH4s4s', rec_packet[0:20]) #0:20
     prot = unpacked_ip[6]
+    addr = inet_ntoa(unpacked_ip[8])
     assert prot == 1
-    icmp_header = unpack('!BBH', rec_packet[20:23] #21:24
+    icmp_header = unpack('!BBH', rec_packet[20:24]) #21:24
     icmp_type = icmp_header[0]
     icmp_code = icmp_header[1]
 
     if (icmp_type == 11 and icmp_code == 0):
-      orig_ip_header = unpack('!BBHHHBBH4s4s', rec_packet[28:47]) #29-48
-      orig_udp_header = unpack('!HHHH', rec_packet[48:55]) #49:56
-      print 'yoyoyo'
+      orig_ip_header = unpack('!BBHHHBBH4s4s', rec_packet[28:48]) #29-48
+      valid_icmp = True
+
+    if(valid_icmp and validate_ip(orig_ip_header, hostip, ipid, ttl)):
+        orig_udp_header = unpack('!HHHH', rec_packet[48:56]) #49:56
+        valid_ip = True
+
+    if(valid_icmp and valid_ip and validate_udp(orig_udp_header)):
+          #this is our packet!
+          return time.time() - time_sent, addr
+    time_left = time_left - timediff
+    if time_left <= 0:
+      raise Exception
+
+def getNewTTL(ttl):
+  return ttl+1
+
+def traceroute(send_socket, recv_socket, hostip, timeout):
+  ttl = 0
+  while True:
+    ttl = getNewTTL(ttl)
+    ipid = randint(1,6535)
+    packet = create_packet(hostip, ttl, ipid)
+    send_socket.sendto(packet, (hostip, 33433))
+    try:
+      response_time, addr = await_response(recv_socket, hostip, ipid, ttl,
+                                            time.time(), timeout)
+      response_time = round(response_time * 1000, 2)
+      print str(ttl) + ": " + str(addr) + " => " + str(response_time) + "ms"
+    except:
+      print str(ttl) + ": " "*"
+
+    if addr == hostip or ttl == 30:
+      break
+
 
 if __name__ == '__main__':
   send_socket = socket(AF_INET, SOCK_RAW, IPPROTO_RAW)
   send_socket.setsockopt(IPPROTO_IP, IP_HDRINCL, 1)
 
+  timeout = 0.75
+
+  print "timeout is set to " + str(timeout) + " seconds per probe"
+
+  setdefaulttimeout(timeout)
   recv_socket = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP)
 
-  '''
-  recv_socket.bind((HOST, 20591))
-  if os.name == 'nt':
-    recv_socket.ioctl(SIO_RCVALL, RCVALL_ON)
-  elif os.name == 'posix':
-    IFF_PROMISC = 0x100
-    SIOCGIFFLAGS = 0x8913
-    SIOCSIFFLAGS = 0x8914
-
-    ifr = ifreq()
-    ifr.ifr_ifrn = "eth0"
-    fcntl.ioctl(recv_socket.fileno(), SIOCGIFFLAGS, ifr)
-    ifr.ifr_flags |= IFF_PROMISC
-    fcntl.ioctl(recv_socket.fileno(), SIOCSIFFLAGS, ifr)
-  else:
-    print "Your OS is not known"
-    sys.exit()
-  '''
-
   hostname = 'www.google.com'
-  hostip = gethostbyname(hostname)
+  try:
+    hostip = gethostbyname(hostname)
+  except:
+    print "Could not resolve hostname"
+    sys.exit()
 
-  packet = create_packet(hostname)
-  print send_socket.sendto(packet, (hostip , 33433))
+  traceroute(send_socket, recv_socket, hostip, timeout)
 
-  print await_response(recv_socket, time.time(), 100)
+  print "Trace complete"
 
 
 
